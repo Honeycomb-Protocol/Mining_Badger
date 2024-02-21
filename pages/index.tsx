5dede6b2670a04bd19631c5249dc587f2078c5ba
Hammad Waseem <56793093+Hammad19@users.noreply.github.com>
2024-02-21 15:52:36 +0500

    Wallet Connection Completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,15 @@
-import Button from "@/components/common/button";
+import Footer from "@/components/footer";
+import WalletConnectButton from "@/components/wallet-context-provider/wallet-connect-button";
 
 export default function Home() {
-  //console connection every time it gets connected
-
-  const { connection } = useConnection();
-  const { select, wallets, publicKey, disconnect } = useWallet();
-  useEffect(() => {
-    if (!connection || !publicKey) {
-      return;
-    }
-
-    console.log("Connection to cluster established:", connection);
-    console.log("Public key:", publicKey.toBase58());
-
-    // Ensure the balance updates after the transaction completes
-  }, [connection, publicKey]);
-
   return (
     <main className="flex flex-col justify-between items-center w-full min-h-[78vh]">
       <div />
       <div className="flex flex-col items-center">
         <h1 className="font-bold text-[50px]">WELCOME TO THE GAME</h1>
-        <Button
-          styles="w-96 mt-12"
-          btnText="Connect Wallet"
-          loading={false}
-          onClick={() => {
-            console.log("hello");
-          }}
-        />
+        <WalletConnectButton />
       </div>
-      <div />
+      <Footer />
     </main>
   );
 }
