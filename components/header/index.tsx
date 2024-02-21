e00e905586341ebc8b0936a59e1e2ff65d44f7eb
Aliyan Hussain <hussainaliyan531@gmail.com>
2024-02-21 12:16:25 +0500

    folder structure modified and bg added

---
+++ components/header/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <img src="/assets/images/logo.png" alt="logo" height="140px" />
+    </div>
+  );
+};
+
+export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import type { AppProps } from "next/app";
 import { K2D } from "next/font/google";
 
 import "@/styles/globals.css";
+import Header from "@/components/header";
 
 const k2d = K2D({ weight: "400", subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={k2d.className}>
+      <Header />
       <Component {...pageProps} />
     </main>
   );
