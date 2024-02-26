618805b4e234e85fdfcc91a383dd13a8de4110d5
Hammad Waseem <56793093+Hammad19@users.noreply.github.com>
2024-02-26 17:36:16 +0500

    Craft Page Completed

--- lib/utils/index.tsx
+++ lib/utils/index.tsx
@@ -1,6 +1,11 @@
 import AllTab from "@/components/home/inventory/all";
 import CraftTab from "@/components/home/tabs/craft";
+import AdamantiteTab from "@/components/home/tabs/craft/tabs/adamantite";
 import BronzeTab from "@/components/home/tabs/craft/tabs/bronze";
+import IronTab from "@/components/home/tabs/craft/tabs/iron";
+import MithrilTab from "@/components/home/tabs/craft/tabs/mithril";
+import RuniteTab from "@/components/home/tabs/craft/tabs/runite";
+import SteelTab from "@/components/home/tabs/craft/tabs/steel";
 import MineTab from "@/components/home/tabs/mine";
 import RefineTab from "@/components/home/tabs/refine";
 import ShopTab from "@/components/home/tabs/shop";
@@ -13,6 +18,18 @@ export const renderCraftTabComponents = async (component: string) => {
           <BronzeTab />
         </>
       );
+    case "Iron":
+      return <IronTab />;
+
+    case "Steel":
+      return <SteelTab />;
+    case "Mithril":
+      return <MithrilTab />;
+
+    case "Adamantite":
+      return <AdamantiteTab />;
+    case "Runite":
+      return <RuniteTab />;
   }
 };
 
